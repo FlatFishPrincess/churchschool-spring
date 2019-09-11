f275d32132e149ea77e271a5f0386baf02edbee2
Jiweon Park <jiweon.park@gracehanin.org>
2019-09-11 16:20:52 -0700

    update department mapper

---
+++ src/main/java/gracehanin/org/churchschool/service/mapper/DepartmentMapper.java
@@ -0,0 +1,21 @@
+package gracehanin.org.churchschool.service.mapper;
+
+import gracehanin.org.churchschool.model.Department;
+import gracehanin.org.churchschool.service.dto.DepartmentDTO;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring") // , uses = {MinistryMapper.class}
+public interface DepartmentMapper extends EntityMapper<DepartmentDTO, Department>{
+
+  @Mapping(source = "ministry.id", target = "ministryId" )
+  @Mapping(source = "ministry.name", target = "ministryName" )
+  // @Mapping(source = "pastorDepartments", target = "pastorDepartmentList" )
+  DepartmentDTO toDto(Department department);
+
+  @Mapping(source = "ministryId", target = "ministry.id")
+  @Mapping(source = "ministryName", target = "ministry.name")
+  // @Mapping(target = "pastorDepartmentList", ignore = true)
+  Department toEntity(DepartmentDTO departmentDTO);
+}
